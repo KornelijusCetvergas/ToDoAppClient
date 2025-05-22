7523af10c8d378d214bb16f08c1033662e2794e4
Kornelijus <kornelijus.cetvergas@gmail.com>
2025-05-22 18:41:01 +0300

    added unordered list of all todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/todo/all")
+      .then(res => res.json())
+      .then((data) => {
+        setTodos(data);
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <span>{todo.description}</span>
+              <button>{todo.isdone ? "Done" : "Pending"}</button>
+            </li>
+          ))}
+        </ul>
     </div>
   );
 }
